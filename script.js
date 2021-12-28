8f2bcd9be5e012f3f59446c1b4a48c39fb273539
evnwttn <evnwttn@gmail.com>
2021-12-28 15:52:23 -0330

    Update script.js

--- script.js
+++ script.js
@@ -27,29 +27,19 @@ class ev02 {
 
         keyDiv.setAttribute("data-key", key.keyCode);
 
-        const keySpan = document.createElement("span");
+        // const keySpan = document.createElement("span");
 
-        if (key.multiClasses !== undefined) {
-          const multiDiv = document.createElement("div");
-          const multiText = key.sound || key.octave || key.multi;
-          keyDiv.appendChild(multiDiv);
-          key.multiClasses.forEach((klass) => {
-            multiDiv.classList.add(klass);
-          });
-          multiDiv.innerHTML = `${multiText}`;
-        }
-
-        if (typeof key.value === "string") {
-          keySpan.innerHTML = key.value;
-        } else if (Array.isArray(key.value)) {
-          key.value.forEach((value) => {
-            const valueDiv = document.createElement("div");
-            valueDiv.innerHTML = value;
-            keySpan.appendChild(valueDiv);
-          });
-        } else {
-          throw new Error(`unknown key.value!: ${key.value}`);
-        }
+        // if (typeof key.value === "string") {
+        //   keySpan.innerHTML = key.value;
+        // } else if (Array.isArray(key.value)) {
+        //   key.value.forEach((value) => {
+        //     const valueDiv = document.createElement("div");
+        //     valueDiv.innerHTML = value;
+        //     keySpan.appendChild(valueDiv);
+        //   });
+        // } else {
+        //   throw new Error(`unknown key.value!: ${key.value}`);
+        // }
 
         keyDiv.appendChild(keySpan);
         rowDiv.appendChild(keyDiv);
@@ -57,136 +47,13 @@ class ev02 {
 
       this.parentDiv.appendChild(rowDiv);
     });
-
-    this.parentDiv.setAttribute("id", `keyboard-${this.keyboardName}`);
-
-    const getKey = (e) => {
-      let location = e.location;
-      let selector;
-      if (location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT) {
-        selector = [
-          '[data-right="true"]',
-          '[data-key="' + e.keyCode + '"]',
-        ].join("");
-      } else {
-        let code = e.keyCode || e.which;
-        selector = [
-          '[data-key="' + code + '"]',
-          '[data-char*="' +
-            encodeURIComponent(String.fromCharCode(code)) +
-            '"]',
-        ].join(",");
-      }
-      return this.parentDiv.querySelector(selector);
-    };
-
-    this.parentDiv.addEventListener("keydown", (e) => {
-      e.preventDefault();
-      let key = getKey(e);
-      if (!key) {
-        return console.warn("No key for", e);
-      }
-      key.setAttribute("data-pressed", "on");
-      this.handleKey(key);
-    });
-
-    this.parentDiv.addEventListener("keyup", (e) => {
-      e.preventDefault();
-      let key = getKey(e);
-      key && key.removeAttribute("data-pressed");
-    });
-
-    this.parentDiv.addEventListener("click", (e) => {
-      let key = e.target;
-      if (key.hasAttribute("data-key") === true) {
-        this.handleKey(key);
-      }
-    });
-
-    this.octave = 4;
-    this.volume = 0;
-    this.oscNum = 0;
-    this.oscType = ["sawtooth", "triangle", "square", "sine"];
-    this.synth = this.makeSynth(this.oscType[this.oscNum]);
   }
 
-  handleKey(key) {
-    this.setSynth(key);
-    this.setOctave(key);
-    this.setVolume(key);
-    this.handleSound(key);
-  }
-
-  makeSynth(synthType) {
-    return new Tone.PolySynth(Tone.Synth, {
-      oscillator: {
-        type: synthType,
-      },
-      volume: this.volume,
-    }).toDestination();
-  }
-
-  setSynth(keyElm) {
-    if (keyElm.hasAttribute("data-osc-up")) {
-      if (this.oscNum <= this.oscType.length - 2) {
-        this.oscNum++;
-        this.synth = this.makeSynth(this.oscType[this.oscNum]);
-      }
-    } else if (keyElm.hasAttribute("data-osc-down")) {
-      if (this.oscNum >= 1) {
-        this.oscNum--;
-        this.synth = this.makeSynth(this.oscType[this.oscNum]);
-      }
-    }
-    this.uiOsc.innerHTML = `[${this.oscType[this.oscNum]}]`;
-  }
-
-  setVolume(keyElm) {
-    if (keyElm.hasAttribute("data-vol-up")) {
-      if (this.volume <= 29) {
-        this.volume++;
-        this.synth = this.makeSynth(this.oscType[this.oscNum]);
-      }
-    } else if (keyElm.hasAttribute("data-vol-down")) {
-      if (this.volume >= -29) {
-        this.volume--;
-        this.synth = this.makeSynth(this.oscType[this.oscNum]);
-      }
-    }
-    this.uiVol.innerHTML = `[${this.volume}db]`;
-  }
-
-  setOctave(keyElm) {
-    if (keyElm.hasAttribute("data-octave-up")) {
-      if (this.octave <= 8) {
-        this.octave++;
-      }
-    } else if (keyElm.hasAttribute("data-octave-down")) {
-      if (this.octave >= 1) {
-        this.octave--;
-      }
-    } else if (keyElm.hasAttribute("data-octave")) {
-      this.octave = keyElm.getAttribute("data-octave");
-    }
-    this.uiOct.innerHTML = `[0${this.octave}]`;
-  }
-
-  handleSound(keyElm) {
-    if (keyElm.hasAttribute("data-sound")) {
-      let note = keyElm.getAttribute("data-sound");
-      this.synth.triggerAttackRelease(`${note}${this.octave}`, "4n");
-    }
-  }
+  /////
 
   getElement() {
     return this.parentDiv;
   }
 }
 
-let keyboardNum = 0;
-container.prepend(new Keyboard(keyboardNum, defaultRows).getElement());
-
-document.getElementById("add-keyboard").onclick = (event) => {
-  keyboardNum++;
-  container.prepend(new Keyboard(keyboardNum, defaultRows).getElement());
-};
+container.prepend(new ev02(inputData).getElement());
