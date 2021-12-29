5635228d2f63b5c9b36c70253a3248494f2ca536
evnwttn <evnwttn@gmail.com>
2021-12-29 13:24:59 -0330

    attys

--- script.js
+++ script.js
@@ -10,8 +10,21 @@ class ev02 {
 
       row.forEach((box) => {
         const boxDiv = document.createElement("div");
+        boxDiv.setAttribute("data-number", box.number);
+        boxDiv.setAttribute("data-working-title", box.workingTitle);
+        boxDiv.setAttribute("data-demo", box.demo);
+        boxDiv.setAttribute("data-guitar", box.guitar);
+        boxDiv.setAttribute("data-bass", box.bass);
+        boxDiv.setAttribute("data-drums", box.drums);
+        boxDiv.setAttribute("data-synths", box.synths);
+        boxDiv.setAttribute("data-fx", box.fx);
+        boxDiv.setAttribute("data-mix", box.mix);
+        boxDiv.setAttribute("data-master", box.master);
+        boxDiv.setAttribute("data-done", box.done);
         boxDiv.classList.add("box");
-        boxDiv.innerHTML = "I'm";
+
+        const boxSpan = document.createElement("span");
+
         rowDiv.appendChild(boxDiv);
       });
 
